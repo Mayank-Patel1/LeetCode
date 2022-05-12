3a3169dd57396a35239f442fa5deb9ebbd57a9ac
Mayank Patel <71153207+Mayank-Patel1@users.noreply.github.com>
2022-05-12 14:44:48 -0400

    Create 338. Counting Bits.java

---
+++ 338. Counting Bits.java
@@ -0,0 +1,12 @@
+// 338. Counting Bits
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] answer = new int[n+1];
+        answer[0] = 0;
+        for(int i=1; i<=n;i++){
+            answer[i] = 1 + answer[i & i-1];
+        }
+        return answer;
+    }
+}
