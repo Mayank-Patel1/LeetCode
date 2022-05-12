04eda7ed4012f62b94a8aede2204579c66db2c33
Mayank Patel <71153207+Mayank-Patel1@users.noreply.github.com>
2022-05-12 14:52:40 -0400

    Update 338. Counting Bits.java

--- 338. Counting Bits.java
+++ 338. Counting Bits.java
@@ -5,7 +5,7 @@ class Solution {
         int[] answer = new int[n+1];
         answer[0] = 0;
         for(int i=1; i<=n;i++){
-            answer[i] = 1 + answer[i & i-1];
+            answer[i] = 1 + answer[i & i-1]; // Dynamic Programming
         }
         return answer;
     }
