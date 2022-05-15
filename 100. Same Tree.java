fd0a13c3ecb26c7c381468973b44a8f34d174762
Mayank Patel <71153207+Mayank-Patel1@users.noreply.github.com>
2022-05-14 20:22:34 -0400

    Create 100. Same Tree.java

---
+++ 100. Same Tree.java
@@ -0,0 +1,11 @@
+// 100. Same Tree
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if(p == null && q == null){ return true; }
+        if(p == null) return false;
+        if(q == null) return false;
+        if(p.val != q.val){ return false; }
+        return( isSameTree(p.right, q.right) && isSameTree(p.left, q.left));
+    }
+}
